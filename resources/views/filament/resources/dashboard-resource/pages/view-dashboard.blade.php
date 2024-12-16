60deed829bc9f54650ffa8477a24c5cd56d61eba
Weerawit Phumphuang <weerawit.pm@gmail.com>
2024-12-16 16:59:25 +0700

    update: slide image

--- resources/views/filament/resources/dashboard-resource/pages/view-dashboard.blade.php
+++ resources/views/filament/resources/dashboard-resource/pages/view-dashboard.blade.php
@@ -3,74 +3,38 @@
     @livewire(\App\Filament\Resources\DashboardResource\Widgets\TableDetail::class)
 
     @if (count($data['images']) > 0)
-        <div id="default-carousel" class="relative w-full" data-carousel="static">
-            <!-- Carousel wrapper -->
-            <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
-                @foreach ($data['images'] as $index => $item)
-                    <div class="{{ $index === 0 ? '' : 'hidden' }} duration-700 ease-in-out" data-carousel-item>
-                        <a href="/storage/{{ $item }}" target="_blank">
-                            <img src="/storage/{{ $item }}"
-                                class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-                                alt="...">
-                        </a>
-                    </div>
-                @endforeach
+        <section id="image-carousel" class="splide" aria-label="Beautiful Images">
+            <div class="splide__track">
+                <ul class="splide__list">
+                    @foreach ($data['images'] as $index => $item)
+                        <li class="splide__slide">
+                            <a href="/storage/{{ $item }}" target="_blank">
+                                <img src="/storage/{{ $item }}" alt="">
+                            </a>
+                        </li>
+                    @endforeach
+                </ul>
             </div>
-            <!-- Slider indicators -->
-            <div class="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
-                @foreach ($data['images'] as $index => $item)
-                    <button type="button" class="w-3 h-3 rounded-full"
-                        aria-current="{{ $index === 0 ? 'true' : 'false' }}" aria-label="Slide {{ $index + 1 }}"
-                        data-carousel-slide-to="{{ $index }}"></button>
-                @endforeach
-            </div>
-            <!-- Slider controls -->
-            <button type="button"
-                class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-                data-carousel-prev>
-                <span
-                    class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                    <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M5 1 1 5l4 4" />
-                    </svg>
-                    <span class="sr-only">Previous</span>
-                </span>
-            </button>
-            <button type="button"
-                class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-                data-carousel-next>
-                <span
-                    class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                    <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="m1 9 4-4-4-4" />
-                    </svg>
-                    <span class="sr-only">Next</span>
-                </span>
-            </button>
-        </div>
+        </section>
+        <style>
+            .splide__slide img {
+                width: 100%;
+                height: 100%;
+                object-fit: cover;
+            }
+        </style>
+
+        <script src="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/js/splide.min.js"></script>
+        <link href="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/css/splide.min.css" rel="stylesheet">
+        <script>
+            document.addEventListener('DOMContentLoaded', function() {
+                new Splide('#image-carousel', {
+                    cover: true,
+                    heightRatio: 0.5,
+                }).mount();
+            });
+        </script>
+
     @endif
 
-    <style>
-        .font-normal {
-            font-weight: 400 !important;
-            text-shadow: none !important;
-            box-shadow: none !important;
-        }
-        .font-medium {
-            font-weight: 500 !important;
-            text-shadow: none !important;
-            box-shadow: none !important;
-        }
-        .shadow-xl {
-            box-shadow: none !important;
-            text-shadow: none !important;
-        }
-    </style>
-
-    <link href="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.css" rel="stylesheet" />
-    <script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.js"></script>
 </x-filament-panels::page>
